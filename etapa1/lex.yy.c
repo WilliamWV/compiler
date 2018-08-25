2152a4b54c8f86dd81348b6cfa92f002f83929e9
wwvargas <william.w.vargas@gmail.com>
2018-08-25 01:53:48 -0300

    Etapa 1 finalizada

--- etapa1/lex.yy.c
+++ etapa1/lex.yy.c
@@ -871,298 +871,266 @@ case 1:
 YY_RULE_SETUP
 #line 32 "scanner.l"
 {
-	#if DEBUG
-		printf("Fim da linha %d\n", get_line_number());
-	#endif
-	++num_lines; // contador de linhas
+	++num_lines;
 }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 38 "scanner.l"
+#line 35 "scanner.l"
 { return TK_PR_INT; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 39 "scanner.l"
+#line 36 "scanner.l"
 { return TK_PR_FLOAT; }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 40 "scanner.l"
+#line 37 "scanner.l"
 { return TK_PR_BOOL; }
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 41 "scanner.l"
+#line 38 "scanner.l"
 { return TK_PR_CHAR; }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 42 "scanner.l"
+#line 39 "scanner.l"
 { return TK_PR_STRING; }
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 43 "scanner.l"
+#line 40 "scanner.l"
 { return TK_PR_IF; }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 44 "scanner.l"
+#line 41 "scanner.l"
 { return TK_PR_THEN; }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 45 "scanner.l"
+#line 42 "scanner.l"
 { return TK_PR_ELSE; }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 46 "scanner.l"
+#line 43 "scanner.l"
 { return TK_PR_WHILE; }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 47 "scanner.l"
+#line 44 "scanner.l"
 { return TK_PR_DO; }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 48 "scanner.l"
+#line 45 "scanner.l"
 { return TK_PR_INPUT; }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 49 "scanner.l"
+#line 46 "scanner.l"
 { return TK_PR_OUTPUT; }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 50 "scanner.l"
+#line 47 "scanner.l"
 { return TK_PR_RETURN; }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 51 "scanner.l"
+#line 48 "scanner.l"
 { return TK_PR_CONST; }
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 52 "scanner.l"
+#line 49 "scanner.l"
 { return TK_PR_STATIC; }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 53 "scanner.l"
+#line 50 "scanner.l"
 { return TK_PR_FOREACH; }
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 54 "scanner.l"
+#line 51 "scanner.l"
 { return TK_PR_FOR; }
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 55 "scanner.l"
+#line 52 "scanner.l"
 { return TK_PR_SWITCH; }
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 56 "scanner.l"
+#line 53 "scanner.l"
 { return TK_PR_CASE; }
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 57 "scanner.l"
+#line 54 "scanner.l"
 { return TK_PR_BREAK; }
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 58 "scanner.l"
+#line 55 "scanner.l"
 { return TK_PR_CONTINUE; }
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 59 "scanner.l"
+#line 56 "scanner.l"
 { return TK_PR_CLASS; }
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 60 "scanner.l"
+#line 57 "scanner.l"
 { return TK_PR_PRIVATE; }
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 61 "scanner.l"
+#line 58 "scanner.l"
 { return TK_PR_PUBLIC; }
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 62 "scanner.l"
+#line 59 "scanner.l"
 { return TK_PR_PROTECTED; }
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 63 "scanner.l"
+#line 60 "scanner.l"
 { return TK_OC_LE; }
 	YY_BREAK
 case 28:
 YY_RULE_SETUP
-#line 64 "scanner.l"
+#line 61 "scanner.l"
 { return TK_OC_GE; }
 	YY_BREAK
 case 29:
 YY_RULE_SETUP
-#line 65 "scanner.l"
+#line 62 "scanner.l"
 { return TK_OC_EQ; }
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 66 "scanner.l"
+#line 63 "scanner.l"
 { return TK_OC_NE; }
 	YY_BREAK
 case 31:
 YY_RULE_SETUP
-#line 67 "scanner.l"
+#line 64 "scanner.l"
 { return TK_OC_AND; }
 	YY_BREAK
 case 32:
 YY_RULE_SETUP
-#line 68 "scanner.l"
+#line 65 "scanner.l"
 { return TK_OC_OR; }
 	YY_BREAK
 case 33:
 YY_RULE_SETUP
-#line 69 "scanner.l"
+#line 66 "scanner.l"
 { return TK_OC_SR; }
 	YY_BREAK
 case 34:
 YY_RULE_SETUP
-#line 70 "scanner.l"
+#line 67 "scanner.l"
 { return TK_OC_SL; }
 	YY_BREAK
 case 35:
 YY_RULE_SETUP
-#line 71 "scanner.l"
+#line 68 "scanner.l"
 { return TK_OC_FORWARD_PIPE; }
 	YY_BREAK
 case 36:
 YY_RULE_SETUP
-#line 72 "scanner.l"
+#line 69 "scanner.l"
 { return TK_OC_BASH_PIPE; }
 	YY_BREAK
 case 37:
 YY_RULE_SETUP
-#line 73 "scanner.l"
+#line 70 "scanner.l"
 { return TK_LIT_FALSE; }
 	YY_BREAK
 case 38:
 YY_RULE_SETUP
-#line 74 "scanner.l"
+#line 71 "scanner.l"
 { return TK_LIT_TRUE; }
 	YY_BREAK
 case 39:
 YY_RULE_SETUP
-#line 75 "scanner.l"
-{
-	#if DEBUG
-		printf("CARACTERE ESPECIAL: %d %c\n", *yytext, *yytext);
-	#endif
-	return *yytext;
-}
+#line 72 "scanner.l"
+{	return *yytext; }
 	YY_BREAK
 case 40:
 YY_RULE_SETUP
-#line 81 "scanner.l"
+#line 73 "scanner.l"
 { return TK_LIT_INT; }
 	YY_BREAK
 case 41:
 YY_RULE_SETUP
-#line 82 "scanner.l"
+#line 74 "scanner.l"
 { return TK_LIT_FLOAT; }
 	YY_BREAK
 case 42:
 YY_RULE_SETUP
-#line 83 "scanner.l"
+#line 75 "scanner.l"
 { return TK_LIT_FLOAT; } // notacao cientifica
 	YY_BREAK
 case 43:
 YY_RULE_SETUP
-#line 84 "scanner.l"
-{ return TK_LIT_CHAR; } //CHARLIT
+#line 76 "scanner.l"
+{ return TK_LIT_CHAR; }
 	YY_BREAK
 case 44:
 YY_RULE_SETUP
-#line 85 "scanner.l"
-{ return TK_IDENTIFICADOR; } //identificadores
+#line 77 "scanner.l"
+{ return TK_IDENTIFICADOR; }
 	YY_BREAK
 case 45:
 YY_RULE_SETUP
-#line 86 "scanner.l"
+#line 78 "scanner.l"
 {return TK_LIT_STRING;}
 	YY_BREAK
 case 46:
 /* rule 46 can match eol */
 YY_RULE_SETUP
-#line 87 "scanner.l"
-{
-	#if DEBUG
-		printf("A line comment: %s\n", yytext);
-	#endif
-	++num_lines;
-}
+#line 79 "scanner.l"
+{ ++num_lines; }
 	YY_BREAK
 case 47:
 /* rule 47 can match eol */
 YY_RULE_SETUP
-#line 93 "scanner.l"
+#line 80 "scanner.l"
 {
+	int chars_in_comment = strlen(yytext), i = 0;
 
-	int chars_in_comment = strlen(yytext);
-	int i = 0;
-
-	#if DEBUG
-
-		printf("A multi-line comment: %s\n", yytext);
-		printf("Number of characters in the comment: %d\n", chars_in_comment);
-
-	#endif
 	for(i=0; i<chars_in_comment; i++){
-		#if DEBUG
-			printf("%c", yytext[i]);
-		#endif
 		if(yytext[i]=='\n'){
 			++num_lines;
 		}
 	}
-	#if DEBUG
-		printf("\n");
-	#endif
-
 }
 	YY_BREAK
 case 48:
 /* rule 48 can match eol */
 YY_RULE_SETUP
-#line 117 "scanner.l"
-{
-	#if DEBUG
-		printf("An whitespace: \"%s\"\n", yytext);
-	#endif
-} // As três expressões regulares anteriores servem para ignorar trechos do código, logo as ações correspondentes devem ser substituídas por "{}" na versão final, ou seja, não retornam tokens
+#line 89 "scanner.l"
+{} // As três expressões regulares anteriores servem para ignorar trechos do código, logo as ações correspondentes devem ser substituídas por "{}" na versão final, ou seja, não retornam tokens
 	YY_BREAK
 case 49:
 YY_RULE_SETUP
-#line 123 "scanner.l"
+#line 91 "scanner.l"
 { return TOKEN_ERRO; }
 	YY_BREAK
 case 50:
 YY_RULE_SETUP
-#line 125 "scanner.l"
+#line 93 "scanner.l"
 ECHO;
 	YY_BREAK
-#line 1166 "lex.yy.c"
+#line 1134 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2167,6 +2135,6 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 125 "scanner.l"
+#line 93 "scanner.l"
 
 
