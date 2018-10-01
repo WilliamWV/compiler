e790c7d7cb87c5a4a67fd36f09371b3206be5b6f
wwvargas <william.w.vargas@gmail.com>
2018-10-01 19:26:29 -0300

    Commit final etapa 3

--- etapa3/test.sh
+++ etapa3/test.sh
@@ -3,9 +3,10 @@ make > dump
 cat correctEntries | ./etapa3 > firstOutput
 cat firstOutput | ./etapa3 > secondOutput
 diff firstOutput secondOutput
+echo $?
 
 ##### Remoção de arquivos temporários #####
 rm -rf dump
 rm -rf firstOutput
 rm -rf secondOutput
-echo $?
+
